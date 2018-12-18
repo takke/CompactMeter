49a5c09c6c17f58c2abc69a32fb04094a21ea68c
takke <hiroaki@v7.com>
2018-12-18 09:59:28 +0900

    tab -> space

--- CompactMeter/worker.h
+++ CompactMeter/worker.h
@@ -3,47 +3,47 @@
 #include "stdafx.h"
 
 struct Traffic {
-	ULONGLONG in;
-	ULONGLONG out;
-	DWORD tick;
+    ULONGLONG in;
+    ULONGLONG out;
+    DWORD tick;
 };
 
 struct CpuUsage {
-	// index=0 が all
-	// index=1 以降が各コアのCPU使用率
-	std::vector<float> usages;
+    // index=0 が all
+    // index=1 以降が各コアのCPU使用率
+    std::vector<float> usages;
 };
 
 class CWorker
 {
 public:
-	CWorker(void);
-	~CWorker(void);
+    CWorker(void);
+    ~CWorker(void);
 
-	void SetParams(HWND hWnd);
-	static DWORD WINAPI ThreadFunc(LPVOID lpParameter);
-	void Terminate();
+    void SetParams(HWND hWnd);
+    static DWORD WINAPI ThreadFunc(LPVOID lpParameter);
+    void Terminate();
 
-	CComCriticalSection criticalSection;
+    CComCriticalSection criticalSection;
 
-	// Network
-	std::vector<Traffic> traffics;
+    // Network
+    std::vector<Traffic> traffics;
 
-	// CPU使用率
-	std::vector<CpuUsage> cpuUsages;
+    // CPU使用率
+    std::vector<CpuUsage> cpuUsages;
 
-	int GetCpuUsage(CpuUsage* out);
+    int GetCpuUsage(CpuUsage* out);
 
 private:
-	HWND hWnd;
+    HWND hWnd;
 
 
-	bool myExitFlag; // 終了指示を保持するフラグ 
-	HANDLE myMutex;  // 排他制御
+    bool myExitFlag; // 終了指示を保持するフラグ 
+    HANDLE myMutex;  // 排他制御
 
-	DWORD WINAPI ExecThread();
-	void CollectCpuUsage(const int &nProcessors, std::vector<PDH_HQUERY> &hQuery, std::vector<PDH_HQUERY> &hCounter, PDH_FMT_COUNTERVALUE &fntValue);
-	boolean InitProcessors(std::vector<PDH_HQUERY> &hQuery, const int &nProcessors, std::vector<PDH_HQUERY> &hCounter);
-	void CollectTraffic();
+    DWORD WINAPI ExecThread();
+    void CollectCpuUsage(const int &nProcessors, std::vector<PDH_HQUERY> &hQuery, std::vector<PDH_HQUERY> &hCounter, PDH_FMT_COUNTERVALUE &fntValue);
+    boolean InitProcessors(std::vector<PDH_HQUERY> &hQuery, const int &nProcessors, std::vector<PDH_HQUERY> &hCounter);
+    void CollectTraffic();
 };
 
