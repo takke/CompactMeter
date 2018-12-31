aec90653523300aca59b69674e7fb1c239c042b8
takke <takke30@gmail.com>
2018-12-31 22:25:03 +0900

    [ci skip]constexpr

--- CompactMeter/Const.h
+++ CompactMeter/Const.h
@@ -3,9 +3,9 @@
 const long MB = 1000 * 1000;
 const long GB = 1000 * 1000 * 1000;
 
-#define MAX_LOADSTRING 100
+constexpr auto MAX_LOADSTRING = 100;
 
-#define PI 3.14159265f
+constexpr auto PI = 3.14159265f;
 
 // タスクトレイ関連
 #define WM_NOTIFYTASKTRAYICON   (WM_USER+100)
