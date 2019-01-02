7219f38b061ea79afc8b671c197120cfd6590e97
takke <takke30@gmail.com>
2019-01-02 10:34:05 +0900

    MeterConfig.h を分離

--- CompactMeter/IniConfig.h
+++ CompactMeter/IniConfig.h
@@ -1,55 +1,7 @@
 #pragma once
 
+#include "MeterConfig.h"
 #include "Const.h"
-
-enum MeterId {
-    METER_ID_UNKNOWN    =   0,
-    METER_ID_CPU        = 100,
-    METER_ID_CORES      = 101,
-    METER_ID_MEMORY     = 200,
-    METER_ID_NETWORK    = 300,
-    METER_ID_DRIVES     = 400,
-};
-
-/**
- * メーターの設定値
- */
-struct MeterConfig {
-    MeterId id;
-    boolean enable;
-//    int backgroundColor;
-
-    MeterConfig(MeterId id_, boolean enable_=true) 
-        : id(id_), enable(enable_)
-    {}
-    MeterConfig() : id(METER_ID_UNKNOWN), enable(false) 
-    {}
-
-    LPCWSTR getName() const {
-
-        switch (id) {
-        case METER_ID_CPU:
-            return L"CPU";
-        case METER_ID_CORES:
-            return L"Core";
-        case METER_ID_MEMORY:
-            return L"Memory";
-        case METER_ID_NETWORK:
-            return L"Network";
-        case METER_ID_DRIVES:
-            return L"Drives";
-        case METER_ID_UNKNOWN:
-        default:
-            return L"Unknown";
-        }
-    }
-
-    template<class Archive>
-    void serialize(Archive & archive)
-    {
-        archive(CEREAL_NVP(id), CEREAL_NVP(enable));
-    }
-};
 
 class IniConfig
 {
